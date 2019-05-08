c8e3ad2d2a10283db8b0331f0944194ace0d71fd
Maheshkumar Rajarathinavel (AZURE) <maheshk@microsoft.com>
2019-05-08 18:16:24 +0530

    updated output

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace mobius
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!-- changes 2");
             });
         }
     }
